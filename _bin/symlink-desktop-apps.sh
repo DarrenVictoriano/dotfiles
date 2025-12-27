3590cf68e32ee3c2cbfc5bbaf7b2d5400b7fad4a
Darren Victoriano <darren.victoriano@gmail.com>
2025-12-26 19:24:52 -0800

    added custom app

--- _bin/symlink-desktop-apps.sh
+++ _bin/symlink-desktop-apps.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 ln -snf "${HOME}/Code/dotfiles/walker/.config/walker/apps/activity-monitor.desktop" "${HOME}/.local/share/applications/activity-monitor.desktop"
+ln -snf "${HOME}/Code/dotfiles/walker/.config/walker/apps/wireless-network-tui.desktop" "${HOME}/.local/share/applications/wireless-network-tui.desktop"
+ln -snf "${HOME}/Code/dotfiles/walker/.config/walker/apps/bluetooth-tui.desktop" "${HOME}/.local/share/applications/bluetooth-tui.desktop"
