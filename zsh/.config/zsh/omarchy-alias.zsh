4162f053ea904539351b51f5687f21800d1140c8
Darren <write@darren.sh>
2025-12-26 19:30:22 -0800

    config-v11 (#30)
    * added custom app
    
    * added custom app
    
    * added custom app

--- zsh/.config/zsh/omarchy-alias.zsh
+++ zsh/.config/zsh/omarchy-alias.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
 # https://github.com/NVIDIA/open-gpu-kernel-modules
+alias icon-path='cd /usr/share/icons/'
 alias update-nvidia='sudo pacman -S linux-headers nvidia-open-dkms nvidia-utils --noconfirm && sudo depmod -a && sudo mkinitcpio -P'
