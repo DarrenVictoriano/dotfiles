2a9bbdc8bf3c2f3801cc4dc84e4ed93ab0520a26
Darren <write@darren.sh>
2026-01-15 19:55:28 -0800

    change cursor (#41)

--- zsh/.config/zsh/vi-mode.zsh
+++ zsh/.config/zsh/vi-mode.zsh
@@ -3,12 +3,20 @@
 # rebind esc to jk
 bindkey -M viins 'jk' vi-cmd-mode
 
-# VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
 VI_MODE_SET_CURSOR=true
-VI_MODE_CURSOR_NORMAL=1
+VI_MODE_CURSOR_NORMAL=2
 VI_MODE_CURSOR_VISUAL=6
-VI_MODE_CURSOR_INSERT=5
+VI_MODE_CURSOR_INSERT=6
 VI_MODE_CURSOR_OPPEND=0
+
+# Cursor Styles
+# 0, 1 - Blinking block
+# 2 - Solid block
+# 3 - Blinking underline
+# 4 - Solid underline
+# 5 - Blinking line
+# 6 - Solid line
 
 # text objects to select qoutes
 autoload -U select-quoted
