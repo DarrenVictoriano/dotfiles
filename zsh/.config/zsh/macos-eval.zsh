12ec90f1d14f4c5c6073377f6abed4472ac87b4a
Darren Victoriano <darren.victoriano@gmail.com>
2026-08-30 15:19:22 -0700

    migrated to mise

--- zsh/.config/zsh/macos-eval.zsh
+++ zsh/.config/zsh/macos-eval.zsh
@@ -2,11 +2,6 @@
 
 export SECONDBRAIN="/Users/darren/Library/Mobile Documents/iCloud~md~obsidian/Documents/SecondBrain"
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path 2>/dev/null)"
-eval "$(pyenv init - 2>/dev/null)"
-
 export VCPKG_DEFAULT_TRIPLET="arm64-osx"
 export VCPKG_ROOT="$HOME/code/cpp/vcpkg"
 export VCPKG_TOOLCHAIN="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
